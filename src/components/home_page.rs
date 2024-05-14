4ca06118b79bfebaa4eb0a9b2c511fde9cbfaf28
knakmitch <mitch.delachevrotitere@knak.com>
2024-05-14 07:38:47 -0400

    update items

--- src/components/home_page.rs
+++ src/components/home_page.rs
@@ -7,6 +7,8 @@ pub fn HomePage() -> impl IntoView {
     // let (count, set_count) = create_signal(0);
     // let on_click = move |_| set_count.update(|count| *count += 1);
 
+    let items: Vec<&str> = vec!["Button Link", "Padding", "Border", "Responsive Options"];
+
     view! {
         <div class="flex flex-col items-center justify-start h-screen w-full bg-gray-200">
             <div class="flex flex-row items-center justify-between w-full bg-gray-800 border-b border-b-gray-950 h-16 px-4">
@@ -18,7 +20,7 @@ pub fn HomePage() -> impl IntoView {
                     // <button class="text-gray-300 bg-gray-700 hover:bg-gray-800 rounded-lg px-4 py-2 border-2 border-amber-700" on:click=on_click>"Click Me: " {count}</button>
                     <div class="flex flex-row w-[800px] h-[460px] bg-gray-700 rounded-xl shadow-xl shadow-gray-800 overflow-hidden">
                         <div class="flex flex-col w-64 border-r border-gray-900">
-                            {["hi", "hello", "how are you", "good", "bad", "what is your name", "what is your age"].iter().map(|item| {
+                            {items.iter().map(|item| {
                                 let strItem = item.to_string();
                                 view! {
                                     <div class="flex flex-row items-center justify-start px-4 py-2 border-b border-gray-900 hover:bg-gray-800 cursor-pointer">
